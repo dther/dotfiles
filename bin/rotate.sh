77b44230f1b0c4e4fec8915ea97a5678c746c5ef
Rudy Dellomas III <rdellomasiii@outlook.com>
2020-06-02 18:47:26 +1000

    Made pixels the size of hams on rotate.sh
    Easier to click that way.

--- bin/rotate.sh
+++ bin/rotate.sh
@@ -1,3 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-echo -e "normal\nflip\nleft\nright" | dmenu -l 10 -c -p "Rotate Screen:" | xargs thinkpad-rotate --force-direction
+#get colors
+. ~/.cache/wal/colors.sh
+
+# color scheme
+nb=$color0
+nf=$color2
+sb=$color2
+sf=$color15
+fn='xos4 Terminus:pixelsize=30'
+
+printf "normal\\nflip\\nleft\\nright" | 
+    dmenu -sf "$sf" -sb "$sb" -nf "$nf" -nb "$nb" -fn "$fn" -l 10 -c -p "Rotate Screen:" | 
+    xargs thinkpad-rotate --force-direction
