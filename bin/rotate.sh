2f6bb4f2d7caff6c4810a428ead97366aa6d7a8c
Rudy Dellomas III <rdellomasiii@outlook.com>
2020-06-02 18:52:00 +1000

    rotate.sh now does nothing if escape is pressed.

--- bin/rotate.sh
+++ bin/rotate.sh
@@ -10,6 +10,8 @@ sb=$color2
 sf=$color15
 fn='xos4 Terminus:pixelsize=30'
 
-printf "normal\\nflip\\nleft\\nright" | 
-    dmenu -sf "$sf" -sb "$sb" -nf "$nf" -nb "$nb" -fn "$fn" -l 10 -c -p "Rotate Screen:" | 
-    xargs thinkpad-rotate --force-direction
+selection=$(printf "normal\\nflip\\nleft\\nright" | 
+    dmenu -sf "$sf" -sb "$sb" -nf "$nf" -nb "$nb" -fn "$fn" -l 10 -c -p "Rotate Screen:") ||
+    exit 1 # do nothing if escape
+
+thinkpad-rotate $selection --force-direction
