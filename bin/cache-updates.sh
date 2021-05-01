86a29fd27ade7293ae743763d33429eba9e5456f
Rudy Dellomas III <rdellomasiii@outlook.com>
2021-05-02 01:21:41 +1000

    Added interrupt for informing dwmblocks

--- bin/cache-updates.sh
+++ bin/cache-updates.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Check if connected to the internet
+ping -c 1 example.org || exit 1
 checkupdates > /home/dther/tmp/checkupdates.current
 count=$(wc -l /home/dther/tmp/checkupdates.current | cut -d' ' -f1)
+pkill -SIGRTMIN+1 dwmblocks
 [ "$count" -eq 0 ] || notify-send "Updates Available:" "$count"
